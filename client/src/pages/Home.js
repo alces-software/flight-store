27c8c2ce2348ff913f699f97866fb10e1086ac7f
Ben Armston <ben.armston@alces-software.com>
2018-09-11 16:32:33 +0100

    Initial overview page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,15 +10,17 @@ import {
   LinkContainer,
   PageHeading,
   Section,
-  SectionIcon,
   makeSection,
 } from 'flight-reactware';
 import FontAwesome from 'react-fontawesome';
 
+import CommunitySiteLink from '../elements/CommunitySiteLink';
 import ContextLink from '../elements/ContextLink';
+import DocsSiteLink from '../elements/DocsSiteLink';
 
 const sections = {
   whatIsIt: makeSection('What is Flight Store?', 'what-is-it', 'pink', 'question'),
+  moreInfo: makeSection('Getting more information', 'more-information', 'blue', 'book'),
 };
 
 const CallToAction = styled(({ children, className, icon, to }) => {
@@ -51,35 +53,47 @@ const Home = () => {
     <div>
       <Container fluid>
         <PageHeading
-          overview="This service provides facilities to purchase Alces Flight
-          HPC clusters."
+          overview="This service has been developed to facilitate aquiring a
+          fully managed High Performance Computing (HPC) cluster."
           sections={Object.values(sections)}
           title="Welcome to the Alces Flight Store."
         />
       </Container>
       <Container>
         <Section
-          overview="The Alces Flight Store service provides access to a store
-          front where you can purchase Alces Flight HPC clusters."
+          overview="The Alces Flight Store service allows you to purchase
+          fully managed Alces Flight HPC clusters complete with comprehensive
+          support."
           section={sections.whatIsIt}
-          title="What is the store service?"
+          title="What is Alces Flight Store?"
         >
           <Row>
             <Col>
-              <SectionIcon name="user" />
-              <h4>
-                Alces Flight Store
-              </h4>
               <p>
-                Alces Flight Store provides a number of cluster packs that can
-                be bought for a monthly fee.  They provide a cluster and fully
-                managed support through Alces Flight Center.
+                Select the HPC cluster pack you want, enter your credit card
+                details and cluster name and you are ready to go!
+              </p>
+              <p>
+                Your cluster will be built by our experienced engineering team
+                in a Virtual Private Cluster (VPC) environment for security,
+                with SSH and graphical-desktop connectivity for users.  Data
+                management tools for POSIX and S3 object storage are also
+                included to help users transfer files and manage storage
+                resources.
+              </p>
+              <p>
+                Your cluster will be fully managed by Alces using the {' '}
                 <ContextLink
                   linkSite="Center"
                   location="/"
                 >
                   Alces Flight Center
-                </ContextLink>.
+                </ContextLink>
+                {' '} service.  Where you will be able to
+                view the dozens of daily checks our engineering team will
+                perform to ensure the health of your cluster request support
+                and advice and access the comprehensive support logs for your
+                cluster.
               </p>
             </Col>
           </Row>
@@ -93,6 +107,23 @@ const Home = () => {
               </CallToAction>
             </Col>
           </Row>
+        </Section>
+        <Section
+          overview="Want to spend some time reading up on Alces Flight Compute
+          prior to starting?"
+          section={sections.moreInfo}
+          title="Getting more information."
+        >
+          <p>
+            We have a{' '}
+            <DocsSiteLink>documentation site</DocsSiteLink> dedicated to the
+            cause as well as a {' '}
+            <CommunitySiteLink>Community Support Portal</CommunitySiteLink>
+            {' '} available for you to join in and read through.
+          </p>
+          <p>
+            Enjoy your flight!
+          </p>
         </Section>
       </Container>
     </div>
