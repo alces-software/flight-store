2c1aa28068c19996d5fffcd57aeb6cf1a36b4969
Ben Armston <ben.armston@alces-software.com>
2019-07-09 15:38:41 +0100

    Remove unused modules

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -3,14 +3,10 @@
 
 import checkout from './checkout';
 import s3Store from './s3Store';
-import session from './session';
 import store from './store';
-// import users from './centerUsers';
 
 export {
   checkout,
   s3Store,
-  session,
   store,
-  // users,
 };
