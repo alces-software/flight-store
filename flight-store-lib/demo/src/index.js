8a3dc5c4fbda354a4d84b05afe5e8c2b5cf46d94
Ben Armston <ben.armston@alces-software.com>
2019-07-22 11:21:35 +0100

    Prevent demo configuration from leaking
    The demo application has some associated configuration which we don't
    want to leak into the library.  In particular, the configuration of
    the SSO cookie name.
    
    Previously, we were using `dotenv` in a manner similar to
    `create-react-app`.  Now we hardcode the configuration in the demo app
    itself.  This prevents the demo configuration from leaking and makes the
    demo easier to understand.

--- flight-store-lib/demo/src/index.js
+++ flight-store-lib/demo/src/index.js
@@ -8,19 +8,16 @@ class Store extends Component {
     return (
       <FlightStore
         // The stripe API key to use
-        stripeApiKey={process.env.REACT_APP_STRIPE_API_KEY}
+        stripeApiKey="pk_test_qetTHATdMis7lA0V4xwcSDGk"
         // Optionally pass the product type to use.  Skipping this presents
         // all product types for the user to select from.
         productType="creditPacks"
-        // The base URL for server that processes the checkouts. E.g.,
-        // http://center.alces-flight.lvh.me:3003
-        apiBaseUrl={process.env.REACT_APP_API_BASE_URL}
-        // The URL prefix for the product definition file. E.g.,
-        // https://alces-flight.s3.amazonaws.com/FlightStore/development-products/
-        productsUrlPrefix={process.env.REACT_APP_PRODUCTS_URL_PREFIX}
-        // The name of the product definition file to use by default. E.g.,
-        // default.json
-        defaultProductsFile={process.env.REACT_APP_DEFAULT_PRODUCTS_FILE}
+        // The base URL for server that processes the checkouts.
+        apiBaseUrl="http://store.alces-flight.lvh.me:4008"
+        // The URL prefix for the product definition file.
+        productsUrlPrefix="https://alces-flight.s3.amazonaws.com/FlightStore/development-products/"
+        // The name of the product definition file to use by default.
+        defaultProductsFile="default.json"
         // The VAT rate to charge.
         vatRate={20}
       />
--- flight-store-lib/nwb.config.js
+++ flight-store-lib/nwb.config.js
@@ -1,24 +1,4 @@
-const dotenv = require('dotenv');
 const webpack = require('webpack');
-
-// Similar to the method used by create-react-app.  Differences are:
-//
-//  - create-react-app supports multipe dot env files
-//  - create-react-app supports loading variables defined outside of the
-//    dotenv file, e.g., exported directly from bash.
-const clientEnv = Object.keys(dotenv.config().parsed)
-  .filter(key => /^REACT_APP_/i.test(key))
-  .reduce(
-    (env, key) => {
-      env[key] = process.env[key];
-      return env;
-    },
-    {
-      // Useful for determining whether we’re running in production mode.
-      // Most importantly, it switches React into the correct mode.
-      NODE_ENV: process.env.NODE_ENV || 'development',
-    }
-  );
 
 module.exports = {
   type: 'react-component',
@@ -36,13 +16,6 @@ module.exports = {
     html: {
       template: 'demo/src/index.html'
     },
-    extra: {
-      plugins: [
-        new webpack.EnvironmentPlugin({
-          ...clientEnv
-        }),
-      ],
-    }
   },
   devServer: {
     host: 'store.alces-flight.lvh.me',
